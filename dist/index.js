465eb548d3c33e590cbd47d97ddb7b5f814344f9
Mahmud Apak <apakmahmud@gmail.com>
2024-04-15 23:58:52 +0200

    TOOK BUILD

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,9 @@ var _express = require('express'); var _express2 = _interopRequireDefault(_expre
 var _boom = require('boom'); var _boom2 = _interopRequireDefault(_boom);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _routes = require('./routes'); var _routes2 = _interopRequireDefault(_routes);
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = _express2.default.call(void 0, );
 
@@ -29,7 +32,23 @@ app.use((err, req, res, next) => {
   }
 });
 
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("notification", (data) => {
+    // Broadcast the notification to all other connected clients
+    socket.broadcast.emit("notification", data);
+  });
+});
+
 const PORT = 4000;
-app.listen(PORT, () =>
+server.listen(PORT, () =>
   console.log("Server is up and running at:", `http://localhost:${PORT}`)
 );
